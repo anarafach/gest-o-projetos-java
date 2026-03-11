ec7f9cbdcb21bec3c3205012d7973b1b849859d2
anarafach <anachiaretti0@gmail.com>
2026-03-11 19:37:15 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+import model.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Usuario usuario = new Usuario(
-                "Ana",
+        Usuario gerente = new Usuario(
+                "Ana Silva",
                 "00000000000",
                 "ana@email.com",
                 "Gerente",
@@ -12,14 +14,14 @@ public class Main {
         );
 
         Projeto projeto = new Projeto(
-                "Sistema de Gestão",
-                "Projeto para gerenciar equipes",
+                "Sistema de Gestão de Projetos",
+                "Sistema para gerenciamento de equipes",
                 "01/03/2026",
                 "30/03/2026",
-                "Em andamento"
+                StatusProjeto.PLANEJADO
         );
 
-        System.out.println("Projeto criado: " + projeto.getNomeProjeto());
-        System.out.println("Responsável: " + usuario.getNome());
+        System.out.println("Projeto criado: " + projeto.getNome());
+        System.out.println("Gerente responsável: " + gerente.getNome());
     }
 }
