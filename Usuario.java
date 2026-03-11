895494b67a8c1e6fb5f09a223ffc21976a172986
anarafach <anachiaretti0@gmail.com>
2026-03-11 19:19:47 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+
+    private String nome;
+    private String cpf;
+    private String email;
+    private String cargo;
+    private String login;
+    private String senha;
+
+    public Usuario(String nome, String cpf, String email, String cargo, String login, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.cargo = cargo;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
