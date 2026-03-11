d6bad3afeb36434a9e02d39c46c1fc57b01069aa
anarafach <anachiaretti0@gmail.com>
2026-03-11 19:36:17 -0300

    Update Tarefa.java

--- Tarefa.java
+++ Tarefa.java
@@ -1,14 +1,24 @@
+package model;
+
 public class Tarefa {
 
-    private String nomeTarefa;
+    private String titulo;
     private String descricao;
     private String status;
     private Usuario responsavel;
 
-    public Tarefa(String nomeTarefa, String descricao, String status, Usuario responsavel) {
-        this.nomeTarefa = nomeTarefa;
+    public Tarefa(String titulo, String descricao, String status, Usuario responsavel) {
+        this.titulo = titulo;
         this.descricao = descricao;
         this.status = status;
         this.responsavel = responsavel;
+    }
+
+    public void atualizarStatus(String status) {
+        this.status = status;
+    }
+
+    public Usuario getResponsavel() {
+        return responsavel;
     }
 }
